fea0bb25865d3b5d194ab3bae3d6a60f6d6eeb5e
shrikw <50625025+shrikw@users.noreply.github.com>
2023-07-25 17:50:06 +0530

    Update NodeStylesPanel.js
    Changes made to change the look of the app
    Current version is 1.0
    
    Signed-off-by: shrikw <50625025+shrikw@users.noreply.github.com>

--- NodeStylesPanel.js
+++ NodeStylesPanel.js
@@ -1,31 +1,42 @@
 // src/NodeStylesPanel.js
-import React from 'react';
-
+//import React from 'react';
+import ReactModal from 'react-modal';
 const shapes = ['ellipse', 'circle', 'database', 'box', 'text'];
 
-function NodeStylesPanel({ labelColors, onLabelColorChange, onLabelShapeChange }) {
+function NodeStylesPanel({ isOpen, onRequestClose, labelColors, onLabelColorChange, onLabelShapeChange }) {
   return (
+    <ReactModal 
+    isOpen={isOpen}
+    onRequestClose={onRequestClose}
+    contentLabel="Node Styles Modal"
+  >
     <div>
       <h2>Node Styles</h2>
-      {Object.entries(labelColors).map(([label, { color, shape }]) => (
-        <div key={label}>
-          <h3>{label}</h3>
-          <div>
-            <label>
-              Color: 
-              <input type="color" value={color} onChange={e => onLabelColorChange(label, e.target.value)} />
-            </label>
-            <label>
-              Shape: 
-              <select value={shape} onChange={e => onLabelShapeChange(label, e.target.value)}>
-                {shapes.map(s => <option key={s} value={s}>{s}</option>)}
-              </select>
-            </label>
+      <div className="node-style">
+        {Object.entries(labelColors).map(([label, { color, shape }]) => (
+          <div key={label}>
+            <h3>{label}</h3>
+            <div>
+              <label>
+                Color: 
+                <input type="color" value={color} onChange={e => onLabelColorChange(label, e.target.value)} />
+              </label>
+              <label>
+                Shape: 
+                <select value={shape} onChange={e => onLabelShapeChange(label, e.target.value)}>
+                  {shapes.map(s => <option key={s} value={s}>{s}</option>)}
+                </select>
+              </label>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
+    </ReactModal>
   );
 }
+
+//export default NodeStylesPanel;
+
 
 export default NodeStylesPanel;
