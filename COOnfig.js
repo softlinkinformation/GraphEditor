106ef51f5de2f5daa63dfa55bb97805f493480fa
Etienne Saliez <etienne@saliez.be>
2023-08-31 12:25:54 +0200

    Update COOnfig.js
    Signed-off-by: Etienne Saliez <etienne@saliez.be>

--- COOnfig.js
+++ COOnfig.js
@@ -1,5 +1,11 @@
 // src/COOnfig.js
+/*
 export const NEO4J_URI = "bolt://localhost:7687";
 export const NEO4J_USER = "neo4j";
 export const NEO4J_PASSWORD = "all4one";
+export const DATABASE = "err";
+*/
+export const NEO4J_URI = "bolt+s://neo4j-dev-bolt.saliez.be:443";
+export const NEO4J_USER = "neo4j";
+export const NEO4J_PASSWORD = "vanilla-love-nova-world-picasso-4896";
 export const DATABASE = "err";
